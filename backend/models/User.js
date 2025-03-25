753159acee282a38741f58d24aa1d019f0deff59
samudra1024 <samudrahosmata@gmail.com>
2025-03-25 23:19:03 +0530

    authroutes.js Modified -> user.js and  Added -> spacing in config.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true,
+		unique: true
+	}
 });
 
 const User = mongoose.model("User", UserSchema);
