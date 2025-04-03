bf6ca0f3309917dff8ae9e2131de4a517647097c
Ranjitha S <ranjithasgowda245@gmail.com>
2025-04-03 22:53:36 +0530

    login page without plugin

--- frontend/DBMS frontend/src/components/LoginPage.tsx
+++ frontend/DBMS frontend/src/components/LoginPage.tsx
@@ -8,15 +8,19 @@ const LoginPage:FunctionComponent = () => {
       			<div className={styles.black} >
       			<div className={styles.loginPageChild} >
 				  <div className={styles.welcomeToStudentContainer}>
-          					<p className={styles.login}>
+				  			
+							<p className={styles.login}>
             						<b className={styles.welcomeTo1}>Welcome to</b>
           					</p>
           					<p className={styles.studentPortal}>student portal</p>
+							 
         				</div>
 				</div>
-      			<img className={styles.unionIcon} alt="" src="Union.svg" />
-      			<img className={styles.unionIcon1} alt="" src="Union.png" />
-      			<img className={styles.unionIcon2} alt="" src="Union.png" />
+				<div>
+								<p><img className={styles.unionIcon} alt="Union Icon" src="/union-1.svg"/></p>
+								<p> <img className={styles.unionIcon1} alt="Union Icon 1" src="/union-2.svg"/></p>
+								<p><img className={styles.unionIcon2} alt="Union Icon 2" src="/union-3.svg"/></p>
+          						</div>
       			<div className={styles.loginBox} >
 				{/* <div className={styles.password}>Password</div>
 				<label className={styles.label}>Password</label>
@@ -26,23 +30,25 @@ const LoginPage:FunctionComponent = () => {
   					placeholder="Enter your password"
 				/> */}
 						
-      			<div className={styles.forgotPassword}>Forgot Password?</div>
         				{/* <img className={styles.loginPageItem} alt="" src="Group 3.svg" /> */}
         				<div className={styles.loginEnterYourContainer}>
           					<p className={styles.login}>
             						<b>Login</b>
+									<b></b>
           					</p>
           					<p className={styles.enterYourAccount}>Enter your account details</p>
+							<br />
+							<br />
         				{/* <img className={styles.loginPageInner} alt="" src="Line 1.svg" />
         				<img className={styles.lineIcon} alt="" src="Line 2.svg" /> */}
-						<div className={styles.username}>Username</div>
+						{/* <div className={styles.username}>Username</div> */}
 						<label className={styles.label}>Username</label>
 						<input
   							type="username"
   							className={styles.inputField}
   							placeholder="Enter your username"
 						/>
-						<div className={styles.password}>Password</div>
+						{/* <div className={styles.password}>Password</div> */}
 						<label className={styles.label}>Password</label>
 						<input
   							type="password"
@@ -51,8 +57,10 @@ const LoginPage:FunctionComponent = () => {
 						/>
         				
         				{/* <div className={styles.rectangleDiv}/> */}
+						<br />
+						<br />
 							<button className={styles.loginButton}>Login</button>
-        					<b className={styles.login1}>Login</b>
+							<div className={styles.forgotPassword}>Forgot Password?</div>
         				</div>
 						</div>
 						</div>
