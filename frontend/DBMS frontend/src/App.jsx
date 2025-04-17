5f936f5122984cbb094026538465ff9e62f07b9b
SAI SAVERSH R <ssr1032005@gmail.com>
2025-04-17 12:47:05 +0530

    samudra changes

--- frontend/DBMS frontend/src/App.jsx
+++ frontend/DBMS frontend/src/App.jsx
@@ -3,10 +3,12 @@ import LoginPage from "./components/LoginPage.jsx";
 import DashBoard from "./components/DashBoard.jsx";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home.jsx";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/dashboard" element={<DashBoard/>} />
     </Routes>
