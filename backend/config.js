6f5118d95f46dbf78b25a49bd2e0c896243b08e4
samudra1024 <samudrahosmata@gmail.com>
2025-04-19 17:58:25 +0530

    added commonforgetpassword.js

--- backend/config.js
+++ backend/config.js
@@ -4,3 +4,9 @@ export const MongoDB = "mongodb+srv://nova:13511@projectnova.3va4f.mongodb.net/L
 
 export const JWT_SECRET = "a137be3c7746482559c528449bbc5d798be455065536ae869a08ee0d58354349"
 
+export const email = {
+    service: 'gmail', // or your email provider
+    username: 'forprojectdbonly@gmail.com', // your email address
+    password: 'ssrp@123', // for Gmail, use App Password if 2FA enabled
+    from: 'samudra' // sender email
+  }
--- frontend/DBMS frontend/src/components/LoginPage.jsx
+++ frontend/DBMS frontend/src/components/LoginPage.jsx
@@ -1,36 +1,34 @@
 import React, { useState } from "react";
 import styles from "./LoginPage.module.css";
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import Spinner from './Spinner.jsx'
-
+import Spinner from "./Spinner.jsx";
 
 const LoginPage = () => {
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
-	const [Loading, setLoading] = useState(false)
-  const [UserType, setUserType] = useState('');
-	// const Nav = useNavigate()
-	const HandlerFunction = () => {
-		const Data = {
-			email,
-			password,
-      UserType
-		}
-		console.log(Data)
-		setLoading(true)
-		axios
-      .post(`http://localhost:5000/api/auth/Student/login` , Data)
-		  .then(res => {
-				console.log(res)
-				setLoading(false)
-		  })
-		  .catch(err => {
-			console.log(err)
-			setLoading(false) 
-		}
-	);
-	}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [Loading, setLoading] = useState(false);
+  const [UserType, setUserType] = useState("");
+  // const Nav = useNavigate()
+  const HandlerFunction = () => {
+    const Data = {
+      email,
+      password,
+      UserType,
+    };
+    console.log(Data);
+    setLoading(true);
+    axios
+      .post(`http://localhost:5000/api/auth/Student/login`, Data)
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
   return (
     <div className={styles.loginPage}>
       <div className={styles.black}>
@@ -55,21 +53,22 @@ const LoginPage = () => {
               className={styles.unionIcon1}
               alt="Union Icon 1"
               src="/union-2.svg"
-              />
+            />
           </p>
           <p>
             <img
               className={styles.unionIcon2}
               alt="Union Icon 2"
               src="/union-3.svg"
-              />
+            />
           </p>
-          <img 
+          <img
             className={styles.student}
-            src="/studentplug.svg" 
-            alt="Student " />
+            src="/studentplug.svg"
+            alt="Student "
+          />
         </div>
-        {Loading ? <Spinner/>: ''}
+        {Loading ? <Spinner /> : ""}
         <div className={styles.loginBox}>
           {/* <div className={styles.password}>Password</div>
 				<label className={styles.label}>Password</label>
@@ -97,7 +96,9 @@ const LoginPage = () => {
               type="Email"
               className={styles.inputField}
               placeholder="Enter your Email"
-			  onChange={(information) => { setEmail(information.target.value)}}
+              onChange={(information) => {
+                setEmail(information.target.value);
+              }}
             />
             {/* <div className={styles.password}>Password</div> */}
             <label className={styles.label}>Password</label>
@@ -105,11 +106,15 @@ const LoginPage = () => {
               type="password"
               className={styles.inputField}
               placeholder="Enter your password"
-			  onChange={(information) => {setPassword(information.target.value)}}
+              onChange={(information) => {
+                setPassword(information.target.value);
+              }}
             />
 
             {/* <div className={styles.rectangleDiv}/> */}
-            <label className={styles.label} htmlFor="type">Choose a Role:</label>
+            <label className={styles.label} htmlFor="type">
+              Choose a Role:
+            </label>
             <select
               id="type"
               name="Types"
@@ -121,8 +126,12 @@ const LoginPage = () => {
               <option value="Faculty">Faculty</option>
               <option value="Organiser">Organiser</option>
             </select>
-            <div className={styles.forgotPassword}><br></br>Forgot Password?</div>
-            <button className={styles.loginButton} onClick={HandlerFunction}>Login</button>
+            <div className={styles.forgotPassword}>
+              <br></br>Forgot Password?
+            </div>
+            <button className={styles.loginButton} onClick={HandlerFunction}>
+              Login
+            </button>
           </div>
         </div>
       </div>
