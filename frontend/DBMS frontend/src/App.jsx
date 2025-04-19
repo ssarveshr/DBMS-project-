e85dce90a249f502098449f5bf3d13cb67ca144c
PraveenKumarM17 <kanniymma@gmail.com>
2025-04-19 13:02:50 +0530

    LoginPage.jsx and LoginPage.module.css were changed in components and app.jsx in the src

--- frontend/DBMS frontend/src/App.jsx
+++ frontend/DBMS frontend/src/App.jsx
@@ -15,3 +15,21 @@ function App() {
   );
 }
 export default App;
+
+
+
+
+
+/* import LoginPage from "./components/LoginPage.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div class = 'app'>
+        <LoginPage />
+      </div>
+    </>
+  );
+}
+export default App; */
