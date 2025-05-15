7a9434d5ebe9293854f346e1db7c713aa9a8df7c
Ranjitha S <ranjithasgowda245@gmail.com>
2025-05-15 09:29:10 +0530

    Organizer dashboard

--- frontend/DBMS frontend/src/main.jsx
+++ frontend/DBMS frontend/src/main.jsx
@@ -3,13 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { AuthProvider } from "./components/navbar/Authcontext.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-      <ToastContainer position="bottom-left" autoClose={3000} />
-    </AuthProvider>
+    <App />
+    <ToastContainer position="bottom-left" autoClose={3000} />
   </BrowserRouter>
 );
